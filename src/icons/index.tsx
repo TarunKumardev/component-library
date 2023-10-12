264057705f9f86ea1002fa0ec80274727e9d937a
tarunkumarf22labs <119585731+tarunkumarf22labs@users.noreply.github.com>
2023-10-12 23:33:04 +0530

    @tarun/buttoncomponent

---
+++ src/icons/index.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const Sahi = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4"><polyline points="9 18 15 12 9 6"></polyline></svg>
+    )
+}
